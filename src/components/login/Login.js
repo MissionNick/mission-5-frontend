fdd9472c25bfcf3bfaf8e4173a53c680532818f0
Jani900 <jani@godojo.fit>
2022-09-08 03:34:07 +1200

    UI signin page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,8 @@
 import styles from "../styles/Login.module.css"
 import { useState } from 'react'
 
-const submitButton = styles;
-const login_container = styles;
-const inputBox = styles;
+const {submitButton,login_container,email,font,password}  = styles;
+
 
  function Login({isLoggedIn,setIsloggedIn}) {
 
@@ -24,14 +23,24 @@ const handleSubmit = (event) => {
   return (
     <div className={login_container}>
       <form onSubmit={handleSubmit}>
-                    <label>
-                          Email:
-                          <input className={inputBox} name="email" type="email" value={inputs.email} onChange={handleChange} />
+                    <label for="email" className={font}>
+                    <br/>
+                    <b>Email</b>
+                    <br/>
+                          <input className={email} name="email" type="email" value={inputs.email} onChange={handleChange} />
                     </label>
-                    <label>
-                          Password:
-                          <input className={inputBox} name= "pwd" type="text" value={inputs.pwd} onChange={handleChange} />
+                    <label className="writing">
+                    <br/>
+                    <b>Password</b>
+                    <br/>
+                          <input className={password} name= "pwd" type="passport" value={inputs.pwd} onChange={handleChange} />
                     </label>
+                    <label className={font}>
+                      <br/>
+                      <input type="checkbox" checked="checked" name="remember" styleName="margin-bottom:15px" /> 
+                      Remember me
+                    </label>
+                    <br/>
             <input className={submitButton} type="submit" value="Submit" />
               </form>
     </div>
